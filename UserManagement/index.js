e5e3edc5bc97a847f809a87d2609ce74d3fc1f28
Himanshu <kashyaphimanshu398@gmail.com>
2021-12-30 00:28:59 +0530

    Schema added

--- UserManagement/index.js
+++ UserManagement/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require("./Schema/userSchema");
+
+mongoose.connect("mongodb://localhost/userdb");
 
 const app = express();
 const port = 3000
+
+run()
+async function run(){
+    const user = new User({username: "himanshu", email: "hhvg@gmail.com"})
+    await user.save();
+    console.log(user);
+}
 
 app.get('*',(req,res) => {
     res.send("HI");
