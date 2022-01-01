8f225c421f52a337057c39371386de7d98ceb38e
Himanshu <kashyaphimanshu398@gmail.com>
2022-01-01 19:14:34 +0530

    signup and login api added

--- UserManagement/Schema/userSchema.js
+++ UserManagement/Schema/userSchema.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     username: String,
-    // password: String,
+    password: String,
     email: String,
-    // email_verified: false 
+    email_verified: Boolean
 })
 
 module.exports = mongoose.model("User",userSchema);
--- UserManagement/index.js
+++ UserManagement/index.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require("./Schema/userSchema");
+const routesUrl = require('./routes/route')
 
 mongoose.connect("mongodb://localhost/userdb");
 
 const app = express();
 const port = 3000
 
-run()
-async function run(){
-    const user = new User({username: "himanshu", email: "hhvg@gmail.com"})
-    await user.save();
-    console.log(user);
-}
+app.use(express.json());
+app.use('/user',routesUrl);
 
 app.get('*',(req,res) => {
-    res.send("HI");
+    res.send("wrong url");
 });
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
